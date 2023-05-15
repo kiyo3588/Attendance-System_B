4dfe41ff84267964a49b99bead1c26d11c6aecaa
kiyo3588 <kiyoohara0627@gmail.com>
2023-05-15 00:51:05 +0000

    6. 2. 1 ログインフォームで復習しよう

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   
   # ログイン機能
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to:'sessions#destroy'
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   
   resources :users
 end
