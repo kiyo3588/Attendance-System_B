5a28382320c11aafb23dd539943b285bc84f3aab
kiyo3588 <kiyoohara0627@gmail.com>
2023-06-18 15:34:10 +0000

    　曜日の色＆分単位の表記変更

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -14,4 +14,11 @@ module AttendancesHelper
   def working_times(start, finish)
     format("%.2f", (((finish - start) / 60) / 60.0))
   end
+  
+  def format_rounded_minutes(time)
+    minutes = time.min
+    rounded_minutes = (minutes / 15) * 15
+    rounded_minutes = [rounded_minutes, 45].min
+    rounded_minutes.to_s.rjust(2, '0')
+  end
 end
